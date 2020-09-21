429ae1aff4191045161e0256910392ec808e85bb
pat-white-code <pat.k.white@gmail.com>
2020-09-21 14:42:40 -0500

    installs axios, makes test button, hits server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const test = async () => {
+    let response = await axios.get('/test');
+    console.log(response);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +16,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={test}> TEST SERVER </button>
         <a
           className="App-link"
           href="https://reactjs.org"
