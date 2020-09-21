e7aed7acad322cc4e49cbd11e966d788dca3685d
pat-white-code <pat.k.white@gmail.com>
2020-09-21 15:08:54 -0500

    app folder

---
+++ app/server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, 'client/build');
+
+app.use(express.static(publicPath));
+
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/test', (req, res)=>{
+  res.send('SERVER HIT!')
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
